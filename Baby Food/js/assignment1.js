e9960ffefbfc31020f10b51018f9124f67b1cfe1
KessNelly <111288175+KessNelly@users.noreply.github.com>
2023-01-13 11:02:28 +0100

    Add files via upload

---
+++ Baby Food/js/assignment1.js
@@ -0,0 +1,8 @@
+function getNameAndPrice(name, price) {
+    let foodName = document.getElementById("food");
+    let foodPrice = document.getElementById("price");
+    
+
+    foodName.value = name;
+    foodPrice.value = price;
+}
